6332a2fddb73fad1091c8a5a3f020d9a95bf921c
mugua <xwang757@gmail.com>
2014-12-10 22:01:12 -0500

    message

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions that are used to create a special object that stores a matrix and cache's its inverse matrix.
+
 
 ## This function creates a special "matrix" object that can cache its inverse.
 
